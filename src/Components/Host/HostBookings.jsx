75668794c7a2fb4fbdf0549b5003afe24cdca8cc
bazil-1854 <bscs22072@itu.edu.pk>
2024-12-07 21:55:01 +0500

    Update HostBookings Component with it's Modal of user

--- src/Components/Host/HostBookings.jsx
+++ src/Components/Host/HostBookings.jsx
@@ -1,7 +1,11 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { FaCheckCircle, FaInfoCircle, FaTimesCircle, FaUserCircle } from "react-icons/fa";
 import { TbBrandBooking } from 'react-icons/tb';
+import { FaUserCircle, FaEnvelope, FaMapMarkerAlt, FaInfoCircle, FaArrowRight } from "react-icons/fa";
+import { AiFillFacebook, AiFillTwitterCircle, AiFillLinkedin, AiOutlineCalendar } from "react-icons/ai";
+import { Link } from 'react-router-dom';
+import { MdClose, MdOutlinePropane } from 'react-icons/md';
+import { GrStatusInfo } from 'react-icons/gr';
 
 
 const HostBookings = () => {
@@ -90,7 +94,7 @@ const HostBookings = () => {
         );
     }
     return (
-        <div className='bg-gray-50 pt-[115px] p-6 min-h-screen justify-center items-center '>
+        <div className='bg-gray-100 pt-[115px] p-6 min-h-screen justify-center items-center '>
             <div className="max-w-[1350px] mx-auto" >
                 <h3 className='text-[20px] md:text-[24px] mb-[15px] text-rose-600 font-[700]'>Applied Bookings For Your Properties:</h3>
                 <div className='h-[2.5px] bg-rose-600 mb-[35px] lg:mb-[55px]'></div>
@@ -98,36 +102,45 @@ const HostBookings = () => {
                     {bookings.map((booking) => (
                         <div
                             key={booking._id}
-                            className="bg-white flex flex-col border rounded-lg p-4 hover:shadow-md transition duration-300 ease-in-out"
+                            className="bg-white flex flex-col border rounded-lg overflow-hidden pb-4 hover:shadow-md transition duration-300 ease-in-out"
                         >
-                            <div className='flex items-center md:mb-0 mb-[15px] space-x-2'>
-                                <div className='w-[35px] h-[35px] md:w-[45px] md:h-[45px] rounded-full flex items-center justify-center text-[28px] text-rose-100 bg-rose-600'>
+                            <div className='flex px-4 pt-3 bg-rose-700 items-center md:mb-0 mb-[15px] space-x-2 pb-[8px] border-b-[2px] border-rose-700 '>
+                                <div className='w-[32px] h-[32px] md:w-[38px] md:h-[38px] rounded-full flex items-center justify-center text-[28px] bg-rose-100 text-rose-600'>
                                     <TbBrandBooking />
                                 </div>
-                                <div className='flex md:flex-row flex-col ml-[15px] md:items-center justify-center'>
-                                    <p className='text-[12px] md:text-[15px] font-[600] text-gray-500 '>Property Name:</p>
-                                    <div className='font-[600] text-[15px] md:text-[18px] md:ml-[10px] text-rose-800'><p>{`${booking.listingDetails.name.slice(0, 35)}...` || 'N/A'}</p></div>
+                                <div className='flex w-[85%] pb-[4px] justify-between items-center'>
+                                    <div className='font-[600] text-[15px] md:text-[22px] text-rose-50'><p>{`${booking.listingDetails.name.slice(0, 35)}..` || 'N/A'}</p></div>
+                                    <p className='text-rose-50 hover:text-red-300 hover:underline cursor-pointer flex items-center '>
+                                        See Listings <FaArrowRight className='pl-[5px] mt-[2px]' />
+                                    </p>
                                 </div>
                             </div>
 
-                            <div className='ml-[53px] flex items-center space-x-2'>
-                                <p className='text-[13px] font-[600] text-gray-500 '>Check-In</p>
-                                <p className='font-[600] text-[14px] text-rose-800'>{new Date(booking.checkIn).toDateString()}</p>
-                            </div>
+                            <div className='ml-[53px] md:mt-[15px]'>
+                                <div className='flex items-center space-x-2'>
+                                    <AiOutlineCalendar className='text-rose-800' />
+                                    <p className='text-[13px] font-[600] text-gray-500 '>Check-In</p>
+                                    <p className='font-[600] text-[14px] text-green-700'>{new Date(booking.checkIn).toDateString()}</p>
+                                </div>
 
-                            <div className='ml-[53px] flex items-center space-x-2'>
-                                <p className='text-[13px] font-[600] text-gray-500 '>Check Out</p>
-                                <p className='font-[600] text-[14px] text-rose-800'>{new Date(booking.checkOut).toDateString()}</p>
-                            </div>
+                                <div className='flex items-center space-x-2'>
+                                    <AiOutlineCalendar className='text-rose-800' />
+                                    <p className='text-[13px] font-[600] text-gray-500 '>Check Out</p>
+                                    <p className='font-[600] text-[14px] text-rose-800'>{new Date(booking.checkOut).toDateString()}</p>
+                                </div>
 
-                            <div className='ml-[53px] flex items-center space-x-2'>
-                                <p className='text-[13px] font-[600] text-gray-500 '>Status</p>
-                                <span className={`font-[600] px-[12px] pb-[1.8px] mt-[5px] text-white rounded-[30px] text-[11px] ${booking.status === 'paid' ? 'bg-green-800' : booking.status === 'pending' ? 'bg-yellow-600' : booking.status === 'confirmed' ? 'bg-blue-800' : booking.status === 'canceled' ? 'bg-red-800' : booking.status === 'completed' ? 'text-gray-800' : ''}`}>{booking.status}</span>
-                            </div>
+                                <div className='flex items-center space-x-2'>
+                                    <MdOutlinePropane className='text-rose-800' />
+                                    <p className='text-[13px] font-[600] text-gray-500 '>Property Type</p>
+                                    <p className='font-[600] text-[14px] text-rose-800'>{booking.listingDetails.property_type}</p>
+                                </div>
 
-                            <div className='ml-[53px] flex items-center space-x-2'>
-                                <p className='text-[13px] font-[600] text-gray-500 '>Property Type</p>
-                                <p className='font-[600] text-[14px] text-rose-800'>{booking.listingDetails.property_type}</p>
+                                <div className='flex  mt-[8px] items-center space-x-2'>
+                                    <GrStatusInfo className='text-rose-800 text-[18px]' />
+                                    <p className='text-[15px] font-[600] text-gray-500 '>Status</p>
+                                    <span className={`scale-[0.9]  mt-[5px] font-[600] px-[12px] pb-[2px] text-white rounded-[30px] text-[11px] ${booking.status === 'approved' ? 'bg-green-800' : booking.status === 'pending' ? 'bg-yellow-600' : booking.status === 'confirmed' ? 'bg-blue-800' : booking.status === 'rejected' ? 'bg-red-800' : booking.status === 'completed' ? 'text-gray-800' : ''}`}>{booking.status}</span>
+                                </div>
+
                             </div>
 
                             <p className="mt-[15px] ml-[53px] flex space-x-3 items-center text-rose-600 cursor-pointer underline" onClick={() => fetchGuestDetails(booking.userID)} >
@@ -136,7 +149,7 @@ const HostBookings = () => {
                             </p>
 
 
-                            <div className="mt-5 md:mt-4 ml-auto">
+                            <div className="mt-5 md:mt-4 ml-auto mr-[8px]">
                                 <button
                                     onClick={() => updateStatus(booking._id, 'rejected')}
                                     className="px-4 py-[4px] mr-2 bg-red-700 text-[13px] text-white rounded-md hover:bg-red-600"
@@ -161,32 +174,88 @@ const HostBookings = () => {
                             {modalLoading ? (
                                 <p>Loading...</p>
                             ) : guestDetails ? (
-                                <>
-                                    <h2 className="text-xl font-semibold mb-4">{guestDetails.username}</h2>
-                                    <p><strong>Email:</strong> {guestDetails.email}</p>
-                                    <p><strong>Location:</strong> {`${guestDetails.location.city}, ${guestDetails.location.country}`}</p>
-                                    <p><strong>Bio:</strong> {guestDetails.bio}</p>
-                                    <p><strong>Social Links:</strong></p>
-                                    <ul className="list-disc pl-5">
-                                        {guestDetails.socialLinks &&
-                                            Object.entries(guestDetails.socialLinks).map(([platform, link]) => (
-                                                <li key={platform}>
-                                                    <a href={link} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">
-                                                        {platform}
-                                                    </a>
+                                <div className='relative'>
+                                    <button onClick={() => setShowModal(false)} className="absolute top-[-5px] right-4 text-gray-500 hover:text-gray-700" aria-label="Close Modal" >
+                                        <MdClose size={24} />
+                                    </button>
+                                    <div className="flex items-center pt-[5px] mb-4">
+                                        <div className="w-11 h-11 bg-gray-300 rounded-full flex-shrink-0 flex items-center justify-center">
+                                            <FaUserCircle size={24} className="text-gray-500" />
+                                        </div>
+                                        <h2 className="text-xl font-semibold ml-4">{guestDetails.username}</h2>
+                                    </div>
+
+                                    {/* Guest Details */}
+                                    <div className="space-y-[13px]">
+                                        <p className="flex items-center gap-2">
+                                            <label className='w-[34px] h-[34px] rounded-full flex items-center justify-center text-[17px] text-rose-100 bg-rose-600'>
+                                                <FaEnvelope />
+                                            </label>
+                                            {guestDetails.email}
+                                        </p>
+                                        <p className="flex items-center gap-2">
+                                            <label className='w-[34px] h-[34px] rounded-full flex items-center justify-center text-[17px] text-rose-100 bg-rose-600'>
+                                                <FaMapMarkerAlt />
+                                            </label>
+                                            <strong>Lives In:</strong> {`${guestDetails.location.city}, ${guestDetails.location.country}`}
+                                        </p>
+                                        <p className="flex items-center gap-2">
+                                            <label className='w-[34px] h-[34px] rounded-full flex items-center justify-center text-[17px] text-rose-100 bg-rose-600'>
+                                                <FaInfoCircle />
+                                            </label>
+                                            <strong>Bio:</strong> {guestDetails.bio}
+                                        </p>
+                                    </div>
+
+                                    {/* Social Links */}
+                                    <div className="mt-4">
+                                        <p className="flex items-center gap-2">
+                                            <FaUserCircle className="text-purple-500" />
+                                            <strong>Social Links:</strong>
+                                        </p>
+                                        <ul className="list-none mt-2 flex gap-4">
+                                            {guestDetails.socialLinks?.facebook && (
+                                                <li>
+                                                    <Link
+                                                        to={guestDetails.socialLinks.facebook}
+                                                        target="_blank"
+                                                        rel="noopener noreferrer"
+                                                        className="text-blue-600 hover:text-blue-800"
+                                                    >
+                                                        <AiFillFacebook size={24} />
+                                                    </Link>
                                                 </li>
-                                            ))}
-                                    </ul>
-                                </>
+                                            )}
+                                            {guestDetails.socialLinks?.twitter && (
+                                                <li>
+                                                    <Link
+                                                        to={guestDetails.socialLinks.twitter}
+                                                        target="_blank"
+                                                        rel="noopener noreferrer"
+                                                        className="text-blue-400 hover:text-blue-600"
+                                                    >
+                                                        <AiFillTwitterCircle size={24} />
+                                                    </Link>
+                                                </li>
+                                            )}
+                                            {guestDetails.socialLinks?.linkedin && (
+                                                <li>
+                                                    <Link
+                                                        to={guestDetails.socialLinks.linkedin}
+                                                        target="_blank"
+                                                        rel="noopener noreferrer"
+                                                        className="text-blue-700 hover:text-blue-900"
+                                                    >
+                                                        <AiFillLinkedin size={24} />
+                                                    </Link>
+                                                </li>
+                                            )}
+                                        </ul>
+                                    </div>
+                                </div>
                             ) : (
                                 <p>No details available.</p>
                             )}
-                            <button
-                                onClick={() => setShowModal(false)}
-                                className="mt-4 px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600"
-                            >
-                                Close
-                            </button>
                         </div>
                     </div>
                 )}
