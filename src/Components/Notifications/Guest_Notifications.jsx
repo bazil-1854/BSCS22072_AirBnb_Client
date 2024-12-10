d1b5a61463546f1e6a58975d2df09c5bd75ebd84
bazil-1854 <bscs22072@itu.edu.pk>
2024-12-10 22:33:00 +0500

    Integrate Fetching of Guest Notificaitons

--- src/Components/Notifications/Guest_Notifications.jsx
+++ src/Components/Notifications/Guest_Notifications.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 
 const GuestNotifications = () => {
     const navigate = useNavigate();
-    const { notifications } = useAuthContext();
+    const { notifications, userNotifications } = useAuthContext();
 
     return (
         <div className='bg-gray-50 pt-[100px] min-h-screen pb-[65px] justify-center items-center '>
@@ -22,7 +22,7 @@ const GuestNotifications = () => {
                     {notifications.map((notification, index) => (
                         <div key={index} className="border-b-[2px] border-rose-700  lg:px-[20px] py-[15px] flex flex-col">
                             <div className="flex items-center">
-                                <div className='w-[32px] h-[32px] md:w-[38px] md:h-[38px] rounded-full flex items-center justify-center text-[28px] bg-rose-100 text-rose-600'>
+                                <div className='w-[32px] h-[32px] md:w-[38px] md:h-[38px] rounded-full flex items-center justify-center text-[28px] bg-rose-800 text-rose-100'>
                                     <TbBrandBooking />
                                 </div>
                                 <h3 className="text-[16px] ml-[5px] text-rose-700 font-[500]">{notification.title} 
@@ -46,6 +46,39 @@ const GuestNotifications = () => {
                                     {notification.UpdatedStatus}
                                 </span>
 
+                                by Host {notification.host}.</p>
+                            <button onClick={() => navigate(`/listing/${notification.listingId}`)} className="ml-[48px] text-rose-700 underline underline-offset-2 font-[500] mt-[4px] text-start">See Listing</button>
+                        </div>
+                    ))}
+
+
+<div className="underline underline-offset-1  text-rose-600 text-center font-[600]">Older Notifications</div>
+{userNotifications.map((notification, index) => (
+                        <div key={index} className="bg-rose-50 rounded-[18px]  lg:px-[20px] py-[15px] flex flex-col">
+                            <div className="flex items-center">
+                                <div className='w-[32px] h-[32px] md:w-[38px] md:h-[38px] rounded-full flex items-center justify-center text-[28px] bg-rose-100 text-rose-600'>
+                                    <TbBrandBooking />
+                                </div>
+                                <h3 className="text-[16px] ml-[5px] text-rose-700 font-[500]">{notification.title} 
+                                <span className={`scale-[0.9] mt-[5px] uppercase font-[600] px-[12px] mx-[8px] pb-[2px] text-white rounded-[30px] text-[11px] 
+                                    ${notification.UpdatedStatus === 'approved' ? 'bg-green-400' :
+                                        notification.UpdatedStatus === 'pending' ? 'bg-yellow-500' :
+                                            notification.UpdatedStatus === 'rejected' ? 'bg-red-300' : 'text-gray-800'}`}
+                                >
+                                    {notification.UpdatedStatus}
+                                </span>
+                                </h3>
+                            </div>
+                            {/*  <p className="ml-[38px]">{notification.details}</p> */}
+
+                            <p className="ml-[48px] break-words text-[14px] mb-[8px] text-rose-600">Your reservation for {notification.address}
+                                between {notification.checkInOut} has been
+                                <span className={`scale-[0.9] mt-[5px] px-[8px] font-[700] underline 
+                                     ${notification.UpdatedStatus === 'approved' ? 'text-green-500' :
+                                        notification.UpdatedStatus === 'rejected' ? 'text-red-500' : 'text-yellow-500'}`}
+                                >
+                                    {notification.UpdatedStatus}
+                                </span>
                                 by Host {notification.host}.</p>
                             <button onClick={() => navigate(`/listing/${notification.listingId}`)} className="ml-[48px] text-rose-700 underline underline-offset-2 font-[500] mt-[4px] text-start">See Listing</button>
                         </div>
